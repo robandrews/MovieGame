b808fafc1768ad6d2210304b36863eaf1eab6f6c
Rob Andrews <rob.andrews@alumni.stanford.edu>
2019-02-02 13:13:48 -0800

    One player, movies only complete!

--- web/src/components/thread.js
+++ web/src/components/thread.js
@@ -3,6 +3,14 @@
 const e = React.createElement;
 
 
+function formatMovieUrl(movieId){
+    return `https://www.imdb.com/title/${movieId}/`
+}
+
+function formatActorUrl(actorId){
+  return `https://www.imdb.com/name/${actorId}/`
+}
+
 class MovieThread extends React.Component {
   constructor(props) {
     super(props);
@@ -18,9 +26,15 @@ class MovieThread extends React.Component {
     });
   }
 
+  resetThread(){
+    this.setState({
+        titles: []
+    });
+  }
+
   render() {
     const listItems = this.state.titles.map((title) =>
-        <li key={title.id}>{title.movie == true ? "Movie" : "Actor"} -- {title.name}{title.year ? `, ${title.year}` : ""}</li>
+        <li key={title.id}>{title.movie == true ? "Movie" : "Actor"} -- {title.name}{title.year ? `, ${title.year}` : ""} [ <a href={title.movie == true ? self.formatMovieUrl(title.id) : self.formatActorUrl(title.id)} target="_blank" >Cheat</a> ]</li>
     );
     return(
       <ul>{listItems}</ul>
